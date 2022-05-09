063bb6402a0d0b230083ab9836acf12da88d9234
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-05-09 18:01:07 -0500

    Add a better GUI by styling buttons and adding functionality through javascript

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
 
+//Open pop up to add books
+var popup = document.querySelector(".pop-up")
 
+function addBook() {
+    popup.classList.add("open-pop-up")
+}
+
+//Close pop up and create eventlistener for document
+var button = document.querySelector(".btn-add");
+document.body.addEventListener('click', function(e) {
+    if (!e.target.classList.contains('btn-add') && !e.target.classList.contains('pop-up') && !e.target.classList.contains('active')) {
+        popup.classList.remove("open-pop-up")
+    }
+});
+
+
+//Add book pop-up function
 
 
 // Book constructor
@@ -13,9 +29,9 @@ function Book(title, author, pages, read) {
     }
 }
 
-// Add book to myLibrary
+// Add book to myLibrary when click submit
 function addBookToLibrary() {
-    
+
 }
 
 //Store books in an array
