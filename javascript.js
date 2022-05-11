7f5ee58c2420a4939651c0265fe8229cd1896449
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-05-11 11:58:23 -0500

    Add functionality to the add books feature for local storage and improve GUI style

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+// Book constructor
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author 
+    this.pages = pages 
+    this.isRead = read
+}
+
+//Store books in an array
+let myLibrary = []
 
 //Open pop up to add books
 var popup = document.querySelector(".pop-up")
@@ -6,38 +16,70 @@ function addBook() {
     popup.classList.add("open-pop-up")
 }
 
+//Close popup function and clear inputs
+var inputs = document.querySelectorAll('.active')
+var checkbox = document.getElementById('isRead')
+
+function closePopup() {
+    popup.classList.remove("open-pop-up");
+    checkbox.checked = false;
+    inputs.forEach(input => {
+        input.value = '';
+    });
+};
+
 //Close pop up and create eventlistener for document
 var button = document.querySelector(".btn-add");
 document.body.addEventListener('click', function(e) {
     if (!e.target.classList.contains('btn-add') && !e.target.classList.contains('pop-up') && !e.target.classList.contains('active')) {
-        popup.classList.remove("open-pop-up")
+        closePopup();
     }
 });
 
-
-//Add book pop-up function
-
-
-// Book constructor
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author 
-    this.pages = pages 
-    this.haveRead = read
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead}`
+//Create a function that retrieves data when click submit
+function retrieveData() {
+    var title = document.getElementById('title').value;
+    var author = document.getElementById('author').value;
+    var pages = document.getElementById('pages').value;
+    var isRead = document.getElementById('isRead').checked;
+    if (title == '' || author == '' || pages == '') {
+        alert('Do not leave fields empty')
+    } else {
+    const newBook = new Book(title, author, pages, isRead)
+    myLibrary.push(newBook)
+    for (var i = 0; i <= myLibrary.length; i++) {
+        console.log(myLibrary[i])
     }
+    closePopup()
+    };
+    myLibrary.forEach(addBookElement)
+};
+
+//Loop through myLibrary and create "books" elements on HTML
+function addBookElement(item, index, array) {
+    const newBookElement = document.createElement('div');
+    const titlePara = document.createElement('p')
+    titlePara.innerText = `"${item.title}"`
+    newBookElement.appendChild(titlePara)
+    const authorPara = document.createElement('p')
+    authorPara.innerText = `${item.author}`
+    newBookElement.appendChild(authorPara)
+    const pagesPara = document.createElement('p')
+    pagesPara.innerText = `item.pages`
+    newBookElement.appendChild(pagesPara)    
+    var gridContainer = document.querySelector('.books-grid');
+    gridContainer.appendChild(newBookElement);
 }
 
-// Add book to myLibrary when click submit
-function addBookToLibrary() {
+//Create callback function for the array.forEach()
 
-}
 
-//Store books in an array
-let myLibrary = [];
 
-//Display on the page for each book
-for (const book in myLibrary) {
 
-}
+
+
+// myLibrary.forEach(addBookElement());
+
+// newBookElement.innerText = `"${newBook.title}"`
+// newBookElement.innerText = `${newBook.author}`
+// newBookElement.innerText = `${newBook.pages}`
